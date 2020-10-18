349e3527a2b9504de6d44b0f62ed7a0e70082216
Bekhzod Turdialiev <bekhzodturdialiev@gmail.com>
2020-10-19 01:08:13 +0500

    adding dist subtree

--- src/services/ContactService.js
+++ src/services/ContactService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import nProgress from "nprogress";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://my-json-server.typicode.com/kiral439/alif-contacts-db",
   withCredentials: false,
   headers: {
     Accept: "application/json",
