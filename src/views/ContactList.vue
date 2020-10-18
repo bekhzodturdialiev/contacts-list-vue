366cc0a71e0e5621e1c1cf0fece8a96a0385be89
Bekhzod Turdialiev <bekhzodturdialiev@gmail.com>
2020-10-18 14:49:22 +0500

    feat: add pagination

--- src/views/ContactList.vue
+++ src/views/ContactList.vue
@@ -96,25 +96,53 @@
                   class="d-lg-flex justify-content-lg-end dataTables_paginate paging_simple_numbers"
                 >
                   <ul class="pagination">
-                    <li class="page-item disabled">
-                      <a class="page-link" href="#" aria-label="Previous"
-                        ><span aria-hidden="true">«</span></a
+                    <template v-if="page !== 1">
+                      <router-link
+                        :to="{
+                          name: 'ContactList',
+                          query: { page: page - 1 }
+                        }"
+                        rel="next"
                       >
-                    </li>
+                        <li
+                          class="page-item"
+                          :class="[page == 1 ? disabled : '']"
+                        >
+                          <a class="page-link" href="#" aria-label="Previous"
+                            ><span aria-hidden="true">«</span></a
+                          >
+                        </li>
+                      </router-link>
+                    </template>
                     <li class="page-item active">
-                      <a class="page-link" href="#">1</a>
+                      <a class="page-link" href="#">{{ page }}</a>
                     </li>
-                    <li class="page-item">
-                      <a class="page-link" href="#">2</a>
-                    </li>
-                    <li class="page-item">
-                      <a class="page-link" href="#">3</a>
-                    </li>
-                    <li class="page-item">
-                      <a class="page-link" href="#" aria-label="Next"
-                        ><span aria-hidden="true">»</span></a
+                    <template v-if="hasNextPage">
+                      <router-link
+                        :to="{
+                          name: 'ContactList',
+                          query: { page: page + 1 }
+                        }"
+                        rel="next"
                       >
-                    </li>
+                        <li class="page-item">
+                          <a class="page-link" href="#">{{ page + 1 }}</a>
+                        </li>
+                      </router-link>
+                      <router-link
+                        :to="{
+                          name: 'ContactList',
+                          query: { page: page + 1 }
+                        }"
+                        rel="next"
+                      >
+                        <li class="page-item">
+                          <a class="page-link" href="#" aria-label="Next"
+                            ><span aria-hidden="true">»</span></a
+                          >
+                        </li>
+                      </router-link>
+                    </template>
                   </ul>
                 </nav>
               </div>
@@ -199,6 +227,9 @@ export default {
     },
     contactEndNumber() {
       return this.contactsTotal % (this.contact.perPage * this.page);
+    },
+    lastPage() {
+      return Math.ceil(this.contactsTotal / this.contact.perPage);
     }
   }
 };
